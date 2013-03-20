2c508390da7687797b4ef4d62e24e68533cec17d
James George <james@jamesgeorge.org>
2013-03-19 23:23:07 -0400

    day update

--- WakeMeCameraFeed/src/testApp.cpp
+++ WakeMeCameraFeed/src/testApp.cpp
@@ -10,25 +10,31 @@ void testApp::setup() {
 	back.setContext(front.getContext());
 	back.setup(1);
 	ghostFrames = 0;
-	
+
+	showPointcloud = false;
 	showRGBD = false;
 	
-	
+	cam1Calib.resize(4);
+	cam2Calib.resize(4);
+
 	timeBetweenTriggers = 6;
 	timeOfNextTrigger = 0;
 	
 	requiredThresholdTime = 7;
 
 	gui.setup("tests");
-    gui.add(xshift.setup("xshift", ofxParameter<float>(), -.15, .15));
-    gui.add(yshift.setup("yshift", ofxParameter<float>(), -.15, .15));
-    gui.add(xsimplify.setup("xsimplify", ofxParameter<float>(), 1, 8));
-    gui.add(ysimplify.setup("ysimplify", ofxParameter<float>(), 1, 8));
-	gui.add(threshold.setup("threshold", ofxParameter<float>(), 15, 255));
-	gui.add(minSize.setup("minSize", ofxParameter<float>(), 1, 640*480));
-	gui.add(minTriggerDepth.setup("minTriggerDepth", ofxParameter<float>(), 50, 255));
-	gui.add(maxTriggerDepth.setup("maxTriggerDepth", ofxParameter<float>(), 50, 255));
+    gui.add(xshift.setup("xshift", ofParameter<float>(), -.15, .15));
+    gui.add(yshift.setup("yshift", ofParameter<float>(), -.15, .15));
+    gui.add(xsimplify.setup("xsimplify", ofParameter<float>(), 1, 8));
+    gui.add(ysimplify.setup("ysimplify", ofParameter<float>(), 1, 8));
+	gui.add(threshold.setup("threshold", ofParameter<float>(), 15, 255));
+	gui.add(minSize.setup("minSize", ofParameter<float>(), 1, 640*480));
+	gui.add(minTriggerDepth.setup("minTriggerDepth", ofParameter<float>(), 50, 255));
+	gui.add(maxTriggerDepth.setup("maxTriggerDepth", ofParameter<float>(), 50, 255));
+	gui.add(autoFire.setup("auto fire", ofParameter<bool>()));
 	
+	rotation = ofQuaternion(0.001134582, 0.9988741, 0.03121687, 0.0357037);
+	translation = ofVec3f(-70.8, -71.8, 2680.2);
 
     gui.loadFromFile("defaultSettings.xml");
 //	ofSetLogLevel(ofxOpenNIContext::LOG_NAME, OF_LOG_VERBOSE);
@@ -36,17 +42,20 @@ void testApp::setup() {
 	
 	renderer1.setup("_calibration");
 	renderer2.setDepthOnly();
+	//renderer1.setDepthOnly("depthCalib1Refined.yml");
+	//renderer2.setDepthOnly("depthCalib2Refined.yml");
+	
 	//renderer1.setup("_calibration");
 	photoPreview.allocate(696, 464, OF_IMAGE_COLOR);
 	renderer1.setRGBTexture(photoPreview);
-	
+	renderer1.setTextureScaleForImage(photoPreview);
 	//renderer2.setDepthOnly();
 	
-	renderer1.setSimplification(ofVec2f(4,4));
-	renderer2.setSimplification(ofVec2f(6,6));
+	renderer1.setSimplification(ofVec2f(5,5));
+	renderer2.setSimplification(ofVec2f(5,5));
 
-	renderer1.farClip = 1500;
-	renderer2.farClip = 1500;
+	renderer1.farClip = 2100;
+	renderer2.farClip = 2100;
 	
 	renderer1.cacheValidVertices = true;
 	renderer2.cacheValidVertices = true;
@@ -60,11 +69,30 @@ void testApp::setup() {
 	cam.autosavePosition = true;
 	cam.loadCameraPosition();
 	
+	
 	contourFinder.setMinAreaRadius(1);
 //	contourFinder.setMaxAreaRadius(100);
 
+	markerColors[0] = ofColor(232,   0, 122);
+	markerColors[1] = ofColor(  0, 180, 255);
+	markerColors[2] = ofColor(212, 255,   0);
+	markerColors[3] = ofColor( 73, 240, 159);
+
 	inThreshold = true;
 	shouldFireCamera = false;
+	
+	ofxXmlSettings settings;
+	settings.loadFile("calibrationPoints.xml");
+	for(int i = 0; i < 4; i++){
+		cam1Calib[i].x = settings.getValue("cam1:x" + ofToString(i), 0);
+		cam1Calib[i].y = settings.getValue("cam1:y" + ofToString(i), 0);
+		cam2Calib[i].x = settings.getValue("cam2:x" + ofToString(i), 0);
+		cam2Calib[i].y = settings.getValue("cam2:y" + ofToString(i), 0);
+	}
+
+	ofxCv::loadMat(rvec, "rotation.yml");
+	ofxCv::loadMat(tvec, "translation.yml");
+	
 }
 
 void testApp::update() {
@@ -103,7 +131,7 @@ void testApp::update() {
 					inThreshold = true;
 				}
 				float timeInThreshold = ofGetElapsedTimef() - thresholdEnteredTime;
-				cout << "time in threshold " << timeInThreshold << endl;
+//				cout << "time in threshold " << timeInThreshold << endl;
 				if(timeInThreshold > requiredThresholdTime){
 					shouldFireCamera = true;
 					inThreshold = false;
@@ -116,6 +144,11 @@ void testApp::update() {
 				}
 			}
 		}
+		
+		if(showRGBD){
+			//renderer1.update();
+		}
+		
 		//cout << "found " << contourFinder.size() << endl;
 		/*
 		for(int y = 0; y < 640; y+=2){
@@ -129,6 +162,9 @@ void testApp::update() {
 	back.update();
 	if(back.isFrameNew()){
 		compressor.convertTo8BitImage(back.getRawDepth(), depthImage2);
+		if(showRGBD){
+			//renderer2.update();
+		}
 	}
 	
 	camera1.update();
@@ -159,10 +195,10 @@ void testApp::update() {
 			m.addStringArg("file://"+currentSaveDirectory);
 			sender.sendMessage(m);
 		}
-		if(showRGBD){
+//		if(showRGBD){
 			photoPreview.setFromPixels(camera1.getPhotoPixels());
 			renderer1.update();
-		}
+//		}
 		inThreshold = false;
 	}
 	
@@ -180,7 +216,8 @@ void testApp::update() {
 		camera2.savePhoto(ofToString(ofGetFrameNum()) + ".jpg");
 	}
 */
-	if(shouldFireCamera){
+	
+	if(autoFire && shouldFireCamera){
 		fireCamera();
 	}
 }
@@ -197,12 +234,26 @@ void testApp::draw() {
 		ofRect(rgbdRect);
 		ofSetColor(255);
 		cam.begin(rgbdRect);
+		
 		renderer1.drawMesh();
+		
+		ofPushMatrix();
+//		ofMultMatrix(calibrationMatrix);
+		ofVec3f axis;
+		float angle;
+		rotation.getRotate(angle, axis);
+		ofRotate(-angle, axis.x,axis.y,axis.z);
+		ofTranslate(-translation);
+		renderer2.drawMesh();
+		ofPopMatrix();
 		cam.end();
 	}
 	else{
 //		front.getColorImage().draw(0,0);
 		//back.getRawIRImage().draw(640, 480);
+		depthImage1Rect = ofRectangle(0,0,640,480);
+		depthImage2Rect = ofRectangle(640,0,640,480);
+
 		if(depthImage1.isAllocated()) depthImage1.draw(0, 0);
 		if(depthImage2.isAllocated()) depthImage2.draw(640, 0);
 		if(inThreshold){
@@ -213,8 +264,17 @@ void testApp::draw() {
 		}
 		contourFinder.draw();
 		ofSetColor(255);
-	}
 		
+		ofPushStyle();
+		for(int i = 0; i < 4; i++){
+			ofSetColor(	markerColors[i] );
+			ofCircle(depthImage1Rect.getTopLeft() + cam1Calib[i], 5);
+			ofCircle(depthImage2Rect.getTopLeft() + cam2Calib[i], 5);
+
+		}
+		ofPopStyle();
+	}
+	
 	gui.draw();
 	
 //	camera2.draw(0, 0);
@@ -237,22 +297,70 @@ void testApp::draw() {
 
 void testApp::keyPressed(int key) {
 	if(key == ' ') {
-		
 		fireCamera();
 	}
 	
-	if(key == '1'){
+	if(key == '-'){
 		showRGBD = !showRGBD;
 		if(showRGBD){
-			photoPreview.setFromPixels(camera1.getPhotoPixels());
-			photoPreview.resize(696, 464);
+			if(camera1.isLiveReady()){
+				photoPreview.setFromPixels(camera1.getPhotoPixels());
+				photoPreview.resize(696, 464);
+			}
 			renderer1.update();
 		}
+	}
+	
+	if(key == 'p'){
+		showPointcloud = !showPointcloud;
+	}
+	if(key == '='){
+		calibrate();
+	}
+	if(key == 't'){
+		saveTestObjs();
+	}
+	if(key == 'y'){
+		saveCombinedMesh("TestCombined.obj");
 	}
 	
 //	if(key == '2'){
 //		camera2.takePhoto();
 //	}
+}
+
+void testApp::mousePressed(int x, int y,int button ){
+//	cout << "mouse pressed!" << endl;
+	
+	ofVec2f camvec(x,y);
+	ofVec2f offset = depthImage1Rect.getTopLeft();
+	if(ofGetKeyPressed('1') && depthImage1Rect.inside(camvec)){
+		cam1Calib[0] = camvec - offset;
+	}
+	if(ofGetKeyPressed('2') && depthImage1Rect.inside(camvec)){
+		cam1Calib[1] = camvec - offset;
+	}
+	if(ofGetKeyPressed('3') && depthImage1Rect.inside(camvec)){
+		cam1Calib[2] = camvec - offset;
+	}
+	if(ofGetKeyPressed('4') && depthImage1Rect.inside(camvec)){
+		cam1Calib[3] = camvec - offset;
+	}
+	
+	offset = depthImage2Rect.getTopLeft();
+	if(ofGetKeyPressed('5') && depthImage2Rect.inside(camvec)){
+		cam2Calib[0] = camvec - offset;
+	}
+	if(ofGetKeyPressed('6') && depthImage2Rect.inside(camvec)){
+		cam2Calib[1] = camvec - offset;
+	}
+	if(ofGetKeyPressed('7') && depthImage2Rect.inside(camvec)){
+		cam2Calib[2] = camvec - offset;
+	}
+	if(ofGetKeyPressed('8') && depthImage2Rect.inside(camvec)){
+		cam2Calib[3] = camvec - offset;
+	}
+
 }
 
 void testApp::fireCamera(){
@@ -272,14 +380,16 @@ void testApp::fireCamera(){
 	ofFile f(path);
 	cout << " absolute path is " << f.getAbsolutePath() << endl;
 	
-	ofMesh mesh1 = renderer1.getReducedMesh(true, ofVec3f(1,1,1), false, true);
-	ofMesh mesh2 = renderer2.getReducedMesh(true, ofVec3f(1,1,1), false, true);
-	ofxObjLoader::save(f.getAbsolutePath() + "/mesh1.obj", mesh1);
-	ofxObjLoader::save(f.getAbsolutePath() + "/mesh2.obj", mesh2);
+	saveCombinedMesh( f.getAbsolutePath() + "/mesh.obj" );
+	//stitch together mesh
+//	ofxObjLoader::save(f.getAbsolutePath() + "/mesh1.obj", mesh1);
+//	ofxObjLoader::save(f.getAbsolutePath() + "/mesh2.obj", mesh2);
+
+//	ofQuaternion rotation(0.001134582, 0.9988741, 0.03121687, 0.0357037);
+//	ofVec3f translation(-70.8, -71.8, 2680.2);
+	
 	front.getColorImage().saveImage(f.getAbsolutePath() + "/backTexture.png");
 	currentSaveDirectory = f.getAbsolutePath();
-	
-	
 	
 	//		ofxOscMessage m;
 	//		m.setAddress("/mesh");
@@ -288,6 +398,152 @@ void testApp::fireCamera(){
 }
 
 //--------------------------------------------------------------
+void testApp::saveCombinedMesh(string path){
+	ofMesh mesh1 = renderer1.getReducedMesh(true, ofVec3f(1,1,1), false, true);
+	ofMesh mesh2 = renderer2.getReducedMesh(true, ofVec3f(1,1,1), false, true);
+	int originalIndeces = mesh1.getNumVertices();
+	ofQuaternion fixedrot;
+	ofVec3f axis;
+	float angle;
+	rotation.getRotate(angle, axis);
+	fixedrot.makeRotate(170, 0,1,0);
+
+	for(int i = 0; i < mesh2.getNumVertices(); i++){
+		mesh1.addVertex( (rotation * mesh2.getVertices()[i]) + translation );
+		mesh1.addTexCoord(ofVec2f(0,0)); //TODO back texture
+	}
+	for(int i = 0; i < mesh2.getNumIndices(); i++){
+		mesh1.addIndex(originalIndeces + mesh2.getIndexPointer()[i]);
+	}
+		
+	ofxObjLoader::save(path, mesh1);
+	
+}
+
+//--------------------------------------------------------------
+void testApp::calibrate(){
+	
+	ofxCv::Calibration cam1CalibBase, cam2CalibBase, cam1CalibRefined, cam2CalibRefined;
+	cam1CalibBase.load("depthCalibBase.yml");
+	cam2CalibBase.load("depthCalibBase.yml");
+	ofxRGBDepthCalibration calibrator;
+//	calibrator.refineDepthCalibration(cam1CalibBase, cam1CalibRefined, &front);
+//	calibrator.refineDepthCalibration(cam2CalibBase, cam2CalibRefined, &back);
+	
+	vector<cv::Point2f> imagePoints1,imagePoints2;
+	
+	for(int i = 0; i < 4; i++){
+		imagePoints1.push_back(ofxCv::toCv(cam1Calib[i]));
+		imagePoints2.push_back(ofxCv::toCv(cam2Calib[i]));
+	}
+	cout << "START CAM CALIB IS " << cam1Calib[0] << endl;
+	
+//	vector<cv::Point2f> ; = ofxCv::toCv(cam2Calib);
+	vector<cv::Point3f> objectPoints1;
+	vector<cv::Point3f> objectPoints2;
+	cout << "object points " << endl;
+	for(int i = 0; i < 4; i++){
+		objectPoints1.push_back( ofxCv::toCv( front.getWorldCoordinateAt(imagePoints1[i].x, imagePoints1[i].y) ));
+		objectPoints2.push_back( ofxCv::toCv(  back.getWorldCoordinateAt(imagePoints2[i].x, imagePoints2[i].y) ));
+		
+		cout << "	" << objectPoints1[i] << " vs " << objectPoints2[i] << endl;
+	}
+	
+	cv::Mat camMatrix1,camMatrix2;
+	cv::Mat distCoeff1,distCoeff2;
+	cam1CalibRefined.getDistortedIntrinsics().getCameraMatrix().copyTo( camMatrix1 );
+	cam2CalibRefined.getDistortedIntrinsics().getCameraMatrix().copyTo( camMatrix2 );
+	cam1CalibRefined.getDistCoeffs().copyTo(distCoeff1);
+	cam2CalibRefined.getDistCoeffs().copyTo(distCoeff2);
+	
+	//cv::Mat affine3d;
+	//cv::Mat inliers;
+	cv::Mat outputMat(3,4,CV_64F);
+	vector<uchar> inliers;
+	cv::estimateAffine3D(objectPoints1, objectPoints2, outputMat, inliers);
+	
+	double* af = outputMat.ptr<double>(0);
+	calibrationMatrix =  ofMatrix4x4(af[0], af[4], af[8], 0.0f,
+									 af[1], af[5], af[9], 0.0f,
+									 af[2], af[6], af[10], 0.0f,
+									 af[3], af[7], af[11], 1.0f);
+	
+//	calibrationMatrix =  ofMatrix4x4(af[0], af[1], af[2], 0.0f,
+//									 af[4], af[5], af[6], 0.0f,
+//									 af[8], af[9], af[10], 0.0f,
+//									 af[3], af[7], af[11], 1.0f);
+	
+//	calibrationMatrix =  ofMatrix4x4(1, 0, 0, 0.0f,
+//									 0, 1, 0, 0.0f,
+//									 0, 0, 1, 0.0f,
+//									 af[3], af[7], af[11], 1.0f);
+		
+	cout << "affine: " << endl << outputMat << endl;
+//	cout << "matrix: " << endl << inliers << endl;
+
+//	cam1CalibRefined.save("depthCalib1Refined.yml");
+//	cam2CalibRefined.save("depthCalib2Refined.yml");
+//	renderer1.setDepthOnly("depthCalib1Refined.yml");
+//	renderer2.setDepthOnly("depthCalib2Refined.yml");
+	
+	saveCalibrationPoints();
+
+//	cv::Mat E,F;
+//	cv::stereoCalibrate(objectPoints, imagePoints1, imagePoints2,
+//						camMatrix1, distCoeff1,
+//						camMatrix2, distCoeff2,
+//						cv::Size(640,480), rvec, tvec, E, F);
+//	cv::solvePnPRansac(objectPoints, imagePoints2, camMatrix2, distCoeff2, rvec, tvec);
+	
+//	ofxCv::saveMat(rvec, "rotation.yml");
+//	ofxCv::saveMat(tvec, "translation.yml");
+	
+//	cv::Mat m3x3;
+//	cv::Rodrigues(rvec, m3x3);
+//	calibrationMatrix.set(1, 0, 0, tvec.at<double>(1,0),
+//						  0, 1, m3x3.at<double>(2,1), tvec.at<double>(2,0),
+//						  0, 0, m3x3.at<double>(2,2), tvec.at<double>(3,0),
+//						  0, 0, 0, 1);
+////	calibrationMatrix.set(m3x3.at<double>(0,0), m3x3.at<double>(1,0), m3x3.at<double>(2,0), tvec.at<double>(1,0),
+////						  m3x3.at<double>(0,1), m3x3.at<double>(1,1), m3x3.at<double>(2,1), tvec.at<double>(2,0),
+////						  m3x3.at<double>(0,2), m3x3.at<double>(1,2), m3x3.at<double>(2,2), tvec.at<double>(3,0),
+////						  0, 0, 0, 1);
+	
+//	calibrationMatrix.getTransposedOf(calibrationMatrix);
+//    calibrationMatrix = ofxCv::makeMatrix(rvec, tvec);
+	
+	cout << "calibrated!" << endl;
+	cout << "rotation: " << endl << rvec << endl;
+	cout << "translation: " << endl << tvec << endl;
+	
+	cout << "START CAM CALIB IS " << cam1Calib[0] << endl;
+//	saveCalibrationPoints();
+}
+
+//--------------------------------------------------------------
 void testApp::exit(){
     gui.saveToFile("defaultSettings.xml");
 }
+
+void testApp::saveCalibrationPoints(){
+	ofxXmlSettings settings;
+	settings.addTag("cam1");
+	settings.addTag("cam2");
+	for(int i = 0; i < 4; i++){
+		settings.setValue("cam1:x" + ofToString(i), cam1Calib[i].x);
+		settings.setValue("cam1:y" + ofToString(i), cam1Calib[i].y);
+		settings.setValue("cam2:x" + ofToString(i), cam2Calib[i].x);
+		settings.setValue("cam2:y" + ofToString(i), cam2Calib[i].y);
+	}
+	settings.saveFile("calibrationPoints.xml");	
+}
+
+void testApp::saveTestObjs(){
+	ofMesh mesh1 = renderer1.getReducedMesh(true, ofVec3f(1,1,1), false, true);
+	ofMesh mesh2 = renderer2.getReducedMesh(true, ofVec3f(1,1,1), false, true);
+	ofxObjLoader::save("Test1.obj", mesh1);
+	ofxObjLoader::save("Test2.obj", mesh2);
+	
+}
+
+
--- WakeMeCameraFeed/src/testApp.h
+++ WakeMeCameraFeed/src/testApp.h
@@ -5,6 +5,7 @@
 #include "ofxDepthImageProviderOpenNI.h"
 #include "ofxDepthImageCompressor.h"
 #include "ofxRGBDCPURenderer.h"
+#include "ofxRGBDepthCalibration.h"
 #include "ofxObjLoader.h"
 #include "ofxOsc.h"
 #include "ofxGameCamera.h"
@@ -17,6 +18,8 @@ public:
 	void draw();
 	void keyPressed(int key);
 	
+	void mousePressed(int x, int y, int button);
+	
 	ofImage pix;
 	ofxEdsdk::Camera camera1;
 //	ofxEdsdk::Camera camera2;
@@ -28,6 +31,9 @@ public:
 	ofxDepthImageCompressor compressor;
 	ofImage depthImage1;
 	ofImage depthImage2;
+	ofRectangle depthImage1Rect;
+	ofRectangle depthImage2Rect;
+	
 	ofxPanel gui;
 	ofxFloatSlider xshift;
     ofxFloatSlider yshift;
@@ -37,18 +43,24 @@ public:
 	ofxFloatSlider minSize;
 	ofxFloatSlider minTriggerDepth;
 	ofxFloatSlider maxTriggerDepth;
-	
+	ofxToggle autoFire;
+
 	ofxRGBDCPURenderer renderer1;
 	ofxRGBDCPURenderer renderer2;
 
 	ofImage photoPreview;
 	ofxOscSender sender;
 	ofxGameCamera cam;
-	bool showRGBD;
 	
+	bool showRGBD;
+	bool showPointcloud;
+	
+	void saveTestObjs();
 	void exit();
 	string currentSaveDirectory;
-	
+	ofQuaternion rotation;
+	ofVec3f translation;
+
 	ofxCv::ContourFinder contourFinder;
 	void fireCamera();
 	
@@ -60,6 +72,18 @@ public:
 	float thresholdEnteredTime;
 	float requiredThresholdTime;
 	int ghostFrames;
+	
+	vector<ofVec2f> cam1Calib;
+	vector<ofVec2f> cam2Calib;
+	
+	ofColor markerColors[4];
+	void calibrate();
+	void saveCalibrationPoints();
+	cv::Mat rvec;
+	cv::Mat tvec;
+	ofMatrix4x4 calibrationMatrix;
+	void saveCombinedMesh(string path);
+	
 };
 
 
